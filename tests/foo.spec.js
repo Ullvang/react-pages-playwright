7be2ac322e673160788ed000fd32fc09100cf6d4
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-13 10:03:58 +0200

    removed failing test, and homepage url

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -12,8 +12,8 @@ test("another basic test", async ({ page }) => {
   await expect(title).toHaveText("Playwright");
 });
 
-test("basic failing test", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  const title = page.locator(".navbar__inner .navbar__title");
-  await expect(title).toHaveText("Playwright22");
-});
+// test("basic failing test", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
+//   const title = page.locator(".navbar__inner .navbar__title");
+//   await expect(title).toHaveText("Playwright22");
+// });
