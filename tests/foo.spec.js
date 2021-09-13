7174b1b625d8d093a7d6ca20d5e6ae4cd38b7595
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-13 11:11:54 +0200

    test local test

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("local", async ({ page }) => {
-  page.goto("/");
+  page.goto("http:localhost:5000");
   const title = page.locator("#title");
   await expect(title).toHaveText("Playwright testing");
 });
