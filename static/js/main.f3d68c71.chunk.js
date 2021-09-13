b5354316631769186e15d139e33fa17b625f7e87
Ullvang <35815114+Ullvang@users.noreply.github.com>
2021-09-13 11:27:23 +0000

    Deploying to gh-pages from @ Ullvang/react-pages-playwright@e7e509b9317ca310deccfddff150f1eb7d0e39b6 🚀

---
+++ static/js/main.f3d68c71.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpplaywright-react-ci-cd"]=this["webpackJsonpplaywright-react-ci-cd"]||[]).push([[0],{11:function(t,e,c){"use strict";c.r(e);var i=c(1),n=c.n(i),r=c(3),s=c.n(r),a=(c(8),c(9),c(0));var d=function(){return Object(a.jsxs)("div",{children:[Object(a.jsx)("h1",{id:"title",children:"Playwright testing"}),Object(a.jsx)("h2",{id:"great",children:"Great222"})]})};s.a.render(Object(a.jsx)(n.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))},8:function(t,e,c){},9:function(t,e,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.f3d68c71.chunk.js.map
