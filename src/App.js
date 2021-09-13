c92a6b9f9dfa1ee6bb41dc0d6ca9d66d9f349b64
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-13 09:55:32 +0200

    adding a failing test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Playwright testing</h1>
-      <h2>Great</h2>
+      <h2>Great222</h2>
     </div>
   );
 }
--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -1,13 +1,19 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test("basic test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Playwright");
 });
 
-test('another basic test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
-  });
+test("another basic test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Playwright");
+});
+
+test("basic failing test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Playwright22");
+});
